984a647d24f9afdd5e0373530ae92428ee8f94f8
henriqcs <casareshenrique@gmail.com>
2020-10-24 14:27:05 -0300

    add container and set button

--- src/ApiSetUrl.jsx
+++ src/ApiSetUrl.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
 import {
-	TextField
+	TextField,
+	Button,
+	Container
 } from "@material-ui/core";
 
 function ApiSetUrl() {
 	return (
 		<>
-			<TextField
-				id="standard-full-width"
-				label="API URL"
-				style={{ margin: 8 }}
-				placeholder="myapiurl:1234"
-				fullWidth
-				margin="normal"
-				InputLabelProps={{
-					shrink: true,
-				}}
-			/>
+			<Container maxWidth="lg">
+				<TextField
+					id="standard-full-width"
+					label="API URL"
+					style={{ margin: 8 }}
+					placeholder="myapiurl:1234"
+					fullWidth
+					margin="normal"
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
+				<Button variant="contained" color="primary">
+					Set API URL
+				</Button>
+			</Container>
 		</>
 	);
 }
